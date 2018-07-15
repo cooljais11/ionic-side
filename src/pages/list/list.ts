1a89250e9298c5f4efaff25743c5088e0cd2a058
jashar <cooljais11@gmail.com>
2018-07-15 10:36:59 +0530

    added search in Community page

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { SelectedCommunityPage } from '../selected-community/selected-community';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'page-list',
@@ -11,6 +12,7 @@ export class ListPage {
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
   items1:Array<{title: string, note: string, icon: string}>;
+  public queryText: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
@@ -25,7 +27,7 @@ export class ListPage {
     this.items = [];
     for (let i = 1; i < 5; i++) {
       this.items.push({
-        title: 'Item ' + i,
+        title: this.icons[i]  + i,
         note: ''+ i,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
@@ -34,7 +36,7 @@ export class ListPage {
     this.items1 = [];
     for (let i = 5; i < 10; i++) {
       this.items1.push({
-        title: 'Item ' + i,
+        title: this.icons[i] + i,
         note: 'item #' + i,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
@@ -56,4 +58,30 @@ export class ListPage {
       refresher.complete();
     }, 1000);
   }
+
+
+  update() {
+    console.log(this.queryText)
+    let queryLower = this.queryText.toLowerCase();
+    let filteredData = [];
+
+    // _.forEach(this.allTeamDivisions, td => {
+    //   let teams = _.filter(td.divisionTeams, t => (<any>t).name.toLowerCase().includes(queryTextLower));
+    //   if (teams.length) {
+    //     filteredTeams.push({ divisionName: td.divisionName, divisionTeams: teams });
+    //   }
+    // });
+
+    _.chain(this.icons)
+      .filter(t => t.toString().toLowerCase().includes(queryLower))
+      .map(t =>
+        filteredData.push({   title: t,
+          note: 'item #' ,
+          icon: this.icons[Math.floor(Math.random() * this.icons.length)]})
+      )
+      .value();
+
+    this.items = filteredData;
+  }
+
 }
